6227703522eb16309db2b0a8c2b04bd727245444
Willem Wannenburg <daeben72@gmail.com>
2024-06-20 15:44:46 +0200

    Added tiers and progress to deckcards (only visual)

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import { View, Text, SafeAreaView, ScrollView, StyleSheet, Button } from "react-native";
 import { Word } from "@/components/CardDeck";
 import BackgroundGradient from "@/components/BackgroundGradient";
 import { router } from "expo-router";
-import DeckCard from "@/components/DeckCard";
+import DeckCard, { Progress, Tier } from "@/components/DeckCard";
 
 const dataSchlafzimmer = require("@/assets/decks/schlafzimmer.json") as Word[];
 const dataWohnzimmer = require("@/assets/decks/wohnzimmer.json") as Word[];
@@ -26,6 +26,8 @@ const database: { [key: string]: Word[] } = {
 
 export default function index() {
   const deckNames = Object.keys(database);
+  const progresses = [3, 0, 2, 4, 1, 0, 3] as Progress[];
+  const tiers = [0, 1, 2, 3, 4, 0, 1] as Tier[];
 
   const toGameScreen = (key: string) => {
     router.navigate({
@@ -44,11 +46,20 @@ export default function index() {
           das. Choose from some of the word decks below.
         </Text>
         <ScrollView style={{ flex: 1 }} contentContainerStyle={styles.scrollContent}>
-          {deckNames.map((deck) => {
-            return <DeckCard key={deck} title={deck} onPress={toGameScreen} />;
+          {deckNames.map((deck, index) => {
+            return (
+              <DeckCard
+                key={deck}
+                title={deck}
+                onPress={toGameScreen}
+                tier={tiers[index]}
+                progress={progresses[index]}
+              />
+            );
           })}
           <DeckCard title="+" onPress={() => {}} />
         </ScrollView>
+        <Button title="Reset" onPress={() => {}} />
       </SafeAreaView>
     </View>
   );
--- components/DeckCard.tsx
+++ components/DeckCard.tsx
@@ -1,12 +1,52 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, ViewStyle } from "react-native";
 import { Word } from "@/types/word";
 
 interface Props {
   title: string;
   onPress: (deck: string) => void;
+  tier?: Tier;
+  progress?: Progress;
 }
 
-export default function DeckCard({ title, onPress }: Props) {
+export type Tier = 0 | 1 | 2 | 3 | 4;
+export type Progress = 0 | 1 | 2 | 3 | 4;
+
+const tierColors = {
+  0: "#219ebc",
+  1: "#023047",
+  2: "#d00000",
+  3: "#ffb703",
+  4: "#fb8500",
+};
+
+const borderWidth = 6;
+
+const getProgressBorder = (
+  tier: Tier | null | undefined,
+  progress: Progress | null | undefined
+) => {
+  if ((tier || tier == 0) && (progress || progress == 0)) {
+    let progressBorder = {
+      borderColor: tierColors[tier],
+    } as ViewStyle;
+
+    // prettier-ignore
+    switch (progress) {
+      // INTENTIONAL FALLTHROUGH!
+      case 0: break;
+      case 4: progressBorder.borderTopWidth = borderWidth;
+      case 3: progressBorder.borderLeftWidth = borderWidth;
+      case 2: progressBorder.borderBottomWidth = borderWidth;
+      case 1: progressBorder.borderRightWidth = borderWidth;
+    }
+
+    return progressBorder;
+  } else {
+    return {};
+  }
+};
+
+export default function DeckCard({ title, onPress, tier, progress }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={0.8}
@@ -17,6 +57,7 @@ export default function DeckCard({ title, onPress }: Props) {
         borderRadius: 15,
         justifyContent: "center",
         alignItems: "center",
+        ...getProgressBorder(tier, progress),
       }}
       onPress={() => onPress(title)}
     >
